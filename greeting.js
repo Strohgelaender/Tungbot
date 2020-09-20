1a1772513e9e99b1182cf87da2f6f3159b5e3fd4
Lucas Welscher <lucas.welscher@nuclos.de>
2020-09-20 20:45:23 +0200

    greeting: updated emotes and ignore warpworld

--- greeting.js
+++ greeting.js
@@ -2,11 +2,11 @@ const {getRandomInt} = require("./util");
 const {say} = require("./bot");
 
 const greetings = ['Moin', 'Moinsen', 'Servus', 'Hey', 'Willkommen im Stream', 'Hi', 'Hallo', 'Grüß dich', 'Tach', 'Grüß Gott', 'Grüezi'];
-const emotes = ['tungdiHype', 'tungdiYoshi', 'tazzHey', 'luckwuHi', 'bytethHi', 'daskueHi', 'daskueHype', 'revedHi', 'sicuiCool', 'nralieSmile', 'PrideHeyyy'];
+const emotes = ['tungdiHype', 'tungdiYoshi', 'tazzHey', 'luckwuHi', 'bytethHi', 'daskueHi', 'daskueHype', 'revedHi', 'sicuiCool', 'akunzaHelloi', 'CoolCat', 'nralieSmile', 'PrideHeyyy'];
 
 const greetedUsers = [];
 
-const ignoredUsers = ['tungdiiltv', 'nightbot', 'streamelements', 'strohgelaender'];
+const ignoredUsers = ['tungdiiltv', 'nightbot', 'streamelements', 'strohgelaender', 'warpworldbot'];
 
 const active = false;
 

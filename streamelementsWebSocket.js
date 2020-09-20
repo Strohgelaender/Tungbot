6d5f1165f566e734f87342d89938dba207459598
Lucas Welscher <lucas.welscher@nuclos.de>
2020-09-20 21:58:57 +0200

    StreamelementsWebSocket: Save updated values

--- streamelementsWebSocket.js
+++ streamelementsWebSocket.js
@@ -8,6 +8,18 @@ const seSocket = io('https://realtime.streamelements.com', {
 });
 
 const followHandlers = [];
+const bitHandlers = [];
+
+const debug = false;
+
+let subscriber;
+let subPoints;
+let follower;
+
+let bitsTotal;
+let bitsMonthly;
+let bitsWeekly;
+let bitsDaily;
 
 async function setupStreamelementsClient() {
 	seSocket.on('connect', () => {
@@ -23,25 +35,60 @@ async function setupStreamelementsClient() {
 		console.log(data);
 	});
 	seSocket.on('event', event => {
-		if (event.type === 'follow') {
-			for (const handler of followHandlers) {
-				handler(event.data);
-			}
+		switch (event.type) {
+			case 'follow':
+				handleEvent(event, followHandlers);
+				break;
+			case 'cheer':
+				handleEvent(event, bitHandlers);
+				break;
 		}
-	})
-	//code for local testing only
-	/*seSocket.on('*', (event, data) => console.log('SE Websocket Event', event, data));
-	const onevent = seSocket.onevent;
-	seSocket.onevent = function (packet) {
-		const args = packet.data || [];
-		onevent.call(this, packet);    // original call
-		packet.data = ["*"].concat(args);
-		onevent.call(this, packet);      // additional call to catch-all
-	};
-	await io.connect();
-	console.log('connected.'); */
-
+	});
+	seSocket.on('event:update', event => {
+		switch (event.name) {
+			case 'subscriber-total':
+				subscriber = event.data.count;
+				break;
+			case 'subscriber-points':
+				subPoints = event.data.count;
+				break;
+			case 'follower-total':
+				follower = event.data.count;
+				break;
+			case 'cheer-total':
+				bitsTotal = event.data.amount;
+				break;
+			case 'cheer-month':
+				bitsMonthly = event.data.amount;
+				break;
+			case 'cheer-week':
+				bitsWeekly = event.data.amount;
+				break;
+			case 'cheer-session':
+				bitsDaily = event.data.amount;
+				break;
+		}
+	});
+	if (debug) {
+		seSocket.on('*', (event, data) => console.log('SE Websocket Event', event, data));
+		const onevent = seSocket.onevent;
+		seSocket.onevent = function (packet) {
+			const args = packet.data || [];
+			onevent.call(this, packet);    // original call
+			packet.data = ["*"].concat(args);
+			onevent.call(this, packet);      // additional call to catch-all
+		};
+		await io.connect();
+	}
+	console.log('SE WebSocket setup finished.');
 }
+
+function handleEvent(event, handlers) {
+	for (const handler of handlers) {
+		handler(event.data);
+	}
+}
+
 exports.setupStreamelementsClient = setupStreamelementsClient;
 exports.onCounter = handler => {
 	seSocket.on('bot:counter', handler);
